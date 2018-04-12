5b529c7fc617443618162962605417a6aa0e0554
Antonio Estevez <tootoast@gmail.com>
2018-04-12 16:37:48 -0400

    Add first module

--- web/modules/custom/atsnj_admin/src/Controller/AtsnjAdminController.php
+++ web/modules/custom/atsnj_admin/src/Controller/AtsnjAdminController.php
@@ -1,14 +1,23 @@
 <?php
 /**
  * @file
- * Contains \Drupal\hello_world\Controller\HelloController.
+ * Contains \Drupal\atsnj_admin\Controller\AtsnjAdminController.
  */
-namespace Drupal\hello_world\Controller;
-class HelloController {
+
+namespace Drupal\atsnj_admin\Controller;
+
+class AtsnjAdminController {
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   */
   public function content() {
     return array(
       '#type' => 'markup',
-      '#markup' => t('Hello, World!'),
+      '#markup' => t('This will be a settings form later'),
     );
   }
+
 }
